9334f44a01678fcf1510f6d2cc1208fa5d87afd6
dbell <greenadvisor@gmail.com>
2019-11-08 22:39:48 -0500

    images added and passing to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import{a} from './images/index'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={a} alt={a}></img>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloa.
         </p>
         <a
           className="App-link"
