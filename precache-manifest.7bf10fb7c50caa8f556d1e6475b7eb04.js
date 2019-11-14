9680c16d94682af2b004390a8a9928ffb45947f9
dbell <greenadvisor@gmail.com>
2019-11-14 18:45:48 -0500

    Updates

---
+++ precache-manifest.7bf10fb7c50caa8f556d1e6475b7eb04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83c4c8365c024e9410b6d85402290cb",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1dcafaa9ffe58e70e0e4",
+    "url": "/clicky-game/static/css/main.5f3a929d.chunk.css"
+  },
+  {
+    "revision": "eb68c72b8efdca53c8c8",
+    "url": "/clicky-game/static/js/2.214a3a19.chunk.js"
+  },
+  {
+    "revision": "1dcafaa9ffe58e70e0e4",
+    "url": "/clicky-game/static/js/main.ff4d6308.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
